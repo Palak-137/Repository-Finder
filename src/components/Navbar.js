1d7853fcfcc5ab2bfcd96a37c63ffff276fac96c
Palak-137 <kumaripalak.aps@gmail.com>
2021-10-04 15:36:44 +0530

    project complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+        <nav class="navbar navbar-expand-lg navbar-dark ">
+  <div class="container-fluid ms-5">
+    <a class="navbar-brand" href="#" style={{fontSize:"3rem"}}>GitHub <i  className="fa fa-github"></i></a>
+  </div>
+</nav>
+        </div>
+    )
+}
+
+export default Navbar
