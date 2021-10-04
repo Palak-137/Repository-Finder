345050c7defd2ae2d2d3b56e7daa35bf87dda686
Palak-137 <kumaripalak.aps@gmail.com>
2021-10-04 15:53:37 +0530

    cleaning the code

--- src/App.js
+++ src/App.js
@@ -7,20 +7,34 @@ import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 function RepoList(props) {
   const repos = props.repos;
   const listItems = repos.map((value,index) =>
+
       <Draggable  key={value.id} draggableId={JSON.stringify(value.id)} index={index}>
+
         {(provided)=>(
             <figure className="col-sm-8 m-4 snip1540" id="snip1540" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} >
-            <div className="profile-image"><a href={value.owner.html_url}><img src={value.owner.avatar_url} alt="profile-sample9" /></a>
+
+            <div className="profile-image">
+              <a href={value.owner.html_url}>
+                  <img src={value.owner.avatar_url} alt="profile-sample9" />
+              </a>
+              
               <div className="icons" style={{color:"white", fontSize: "2rem"}}>
-                <a href={value.html_url}  style={{color:"white", fontSize: "2rem"}}><i className="fa fa-github" style={{fontSize: "2rem"}}></i></a>
+                <a href={value.html_url}  style={{color:"white", fontSize: "2rem"}}>
+                  <i className="fa fa-github" style={{fontSize: "2rem"}}></i>
+                </a>
               </div>
+            
             </div>
             <figcaption>
+
               <h3>{value.name}</h3>
               <h4>By {value.owner.login}</h4>
               <p>{value.description}</p>
+
             </figcaption>
+
             {provided.placeholder}
+
           </figure>
         )}
       </Draggable>
@@ -35,10 +49,11 @@ function RepoList(props) {
 
 
 function App() {
+
   const [data, setData] = useState([]);
-  const [filterdata, setfilterdata] = useState([]);
   const [search, setSearch] = useState('')
   const [characters, updateCharacters] = useState(data);
+
 
   useEffect(() => {
     var myHeaders = new Headers();
@@ -55,10 +70,10 @@ function App() {
       .then(response => response.json())
       .then(result => {
          setData(result);
-         setfilterdata(result);
          updateCharacters(result);
         })
       .catch(error => console.log('error', error));
+
   },[]);
 
 
@@ -68,6 +83,7 @@ function App() {
         const postName = repo.name.toLowerCase();
         return postName.includes(query);
     });
+
 };
 
   function handleChange(e){
@@ -80,12 +96,10 @@ function App() {
     }else{
       updateCharacters(list);
     }
-   
-    
-    
   }
 
   function handleDrag(result) {
+
     if (!result.destination) return;
 
     const items = Array.from(characters);
@@ -100,33 +114,47 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+
       <div className="container mt-5">
+
         <form class="d-flex ms-5 me-4 ms-5 search">
-            <input class="form-control me-2" type="search"  aria-label="Search" 
-            placeholder="Search name" 
-            value={search}
-            onChange={(e)=>{handleChange(e)}}/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+
+              <input class="form-control me-2" type="search"  aria-label="Search" 
+              placeholder="Search name" 
+              value={search}
+              onChange={(e)=>{handleChange(e)}}/>
+              
+              <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+
             <div className="container boxes" style={{width:"80%", alignItems:"center"}}>
 
               <DragDropContext onDragEnd={handleDrag}>
+
                 <Droppable droppableId="characters">
+
                   {(provided)=>(
-                      <div id="characters" className="row m-5 mt-5"  style={{alignItems:"center"}} {...provided.droppableProps} ref={provided.innerRef}>
+                      <div id="characters" className="row m-5 mt-5"  
+                      style={{alignItems:"center"}} 
+                      {...provided.droppableProps} 
+                      ref={provided.innerRef}>
+                      
                       <RepoList repos={characters}/>
+
                       {provided.placeholder}
+
                     </div>
                   )}
 
                   
-                  </Droppable>
+                </Droppable>
+
               </DragDropContext>
               
               </div>
               
-                </div>
-              </div>
+          </div>
+      </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,14 @@ import React from 'react'
 function Navbar() {
     return (
         <div>
-        <nav class="navbar navbar-expand-lg navbar-dark ">
-  <div class="container-fluid ms-5">
-    <a class="navbar-brand" href="#" style={{fontSize:"3rem"}}>GitHub <i  className="fa fa-github"></i></a>
-  </div>
-</nav>
+          <nav className="navbar navbar-expand-lg navbar-dark ">
+            <div className="container-fluid ms-5">
+             <a className="navbar-brand" href="https://github.com/" style={{fontSize:"3rem"}}>
+                  GitHub 
+                  <i  className="fa fa-github"></i>
+               </a>
+            </div>
+          </nav>
         </div>
     )
 }
